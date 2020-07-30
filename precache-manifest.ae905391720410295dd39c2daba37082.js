55d336f071019c5bfff4aecfed2f88df6f4dc32c
DawidMmM <danonim56@gmail.com>
2020-07-30 19:02:49 +0200

    Updates

---
+++ precache-manifest.ae905391720410295dd39c2daba37082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd6a5039c96bb32e64af400543810df",
+    "url": "/polluted-cit/index.html"
+  },
+  {
+    "revision": "4eed8529c650b4ea32e3",
+    "url": "/polluted-cit/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4eed8529c650b4ea32e3",
+    "url": "/polluted-cit/static/js/2.0bdac76c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/polluted-cit/static/js/2.0bdac76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a5caf779cb2f7f4eba",
+    "url": "/polluted-cit/static/js/main.bd4e4c2b.chunk.js"
+  },
+  {
+    "revision": "0400045edfd512bb8b80",
+    "url": "/polluted-cit/static/js/runtime-main.45037b09.js"
+  }
+]);
